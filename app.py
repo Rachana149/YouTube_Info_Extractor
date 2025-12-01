6109515608368431d3d5c111c3d64e53853a5af4
Rachana Mahato💕. <rachanamahato24@gmail.com>
2025-12-01 17:27:33 +0530

    Fixed channel handle detection

--- app.py
+++ app.py
@@ -63,24 +63,36 @@ if not st.session_state.start_dashboard:
 
 
 
-def extract_channel_id(url, youtube=None):
+def extract_channel_id(url, youtube):
     try:
         url = url.strip()
+
+        # Case 1: direct channel link
         if "/channel/" in url:
-            return url.split("/channel/")[-1].split("?")[0]
-        if "/@" in url and youtube:
-            username = re.findall(r"/@([A-Za-z0-9_-]+)", url)
-            if username:
-                res = youtube.search().list(
-                    part="snippet", q=username[0], type="channel", maxResults=1
-                ).execute()
-                if res.get("items"):
-                    return res["items"][0]["snippet"]["channelId"]
-        if re.match(r"^[A-Za-z0-9_-]{24}$", url):
+            return url.split("channel/")[1].split("?")[0]
+
+        # Case 2: Handle format (@username)
+        if "@" in url:
+            handle = url.split("@")[1]  
+
+            req = youtube.channels().list(
+                part="id",
+                forHandle=handle
+            ).execute()
+
+            if req.get("items"):
+                return req["items"][0]["id"]
+
+        # Case 3: direct Channel ID
+        if re.match(r"UC[A-Za-z0-9_-]{22}", url):
             return url
+        
         return None
-    except:
+
+    except Exception as e:
+        print("Error extracting ID:", e)
         return None
+
 
 
 def get_uploads_playlist_id(channel_id, youtube):
